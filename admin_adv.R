34ef4e7a66518c7355af0d563590c7408b5c3f69
sinemdalkilic <sinemdalkilic@gmail.com>
2018-04-01 12:46:35 +0300

    Adding lags, corrected duplicate data

--- admin_adv.R
+++ admin_adv.R
@@ -13,18 +13,19 @@ sellers<-readRDS("C://Users//Acerr//Desktop//Courses//Current//IE 492//CAT 42-20
 tr[,price:=amount/quantity]
 setnames(tr,"day", "date")
 setnames(inventory,"day", "date")
-products[,pv:=app_pv+pc_pv]
-products[,uv:=app_uv+pc_uv]
+products[,pvs:=app_pv+pc_pv]
+products[,uvs:=app_uv+pc_uv]
 tr_sub_cat=merge(tr[,.(if_cainiao, merchant_id, date, item_id, quantity, amount, price),], 
-                 products[, .(date, item_id, sub_category_id, pv, uv, brand_id)], by=c("date", "item_id"))
+                 products[, .(date, item_id, sub_category_id, pvs, uvs, brand_id)], by=c("date", "item_id"))
 
-#inventory_v1=inventory[, .(beg_inv=sum(beg_inv)), by=c("item_id", "date")]
+
 inventory_availability=inventory[, .(availability=sum(beg_inv)+sum(replen_in)), by=c("item_id", "date")]
 # inventory_availability=inventory_availability[availability==0, if_avail:=0 ]
 # inventory_availability=inventory_availability[availability!=0, if_avail:=1 ]
 #tr_sc=merge(tr_sub_cat,inventory_availability[,.(item_id,date,if_avail=if_avail)],by=c("date", "item_id"))
 tr_sc=merge(tr_sub_cat,inventory_availability[,.(item_id,date,availability)],by=c("date", "item_id"))
-tr_sc=tr_sc[, view:=uv/pv,]
+#tr_sc[, view:=uv/pv,]
+#tr_sc[,viewmult:=uv*pv,]
 
 setnames(sellers, "subcategory_id","sub_category_id")
 setnames(sellers, "day", "date")
@@ -36,64 +37,69 @@ scores_sc_brand=merch_brand[,.(logisticscore=mean(avg_logistic_review_score,na.r
 
 tr_sc=merge(scores_sc_brand, tr_sc, by=c("date", "brand_id", "sub_category_id"))
 
-dummy<-tr_sc
 
-dummy<-dummy[availability<950,dummy:=1,by=c("date", "sub_category_id", "brand_id")]
-dummy<-dummy[availability>=950,dummy:=0,by=c("date", "sub_category_id", "brand_id")]
-unique(dummy[,.(merchantnumless950:=sum(dummy)),by=c("brand_id","date","sub_category_id")])
+tr_sc[availability<950,dummy:=1,by=c("date", "sub_category_id", "brand_id")]
+tr_sc[availability>=950,dummy:=0,by=c("date", "sub_category_id", "brand_id")]
+tr_sc[,avgavailability:=mean(availability),by=c("date", "sub_category_id")]
+tr_sc<-unique(tr_sc[,merchnumless950:=sum(dummy),by=c("brand_id","date","sub_category_id")])
 
-tr_sc_brand=tr_sc[,.(quantity=sum(quantity),price=mean(price),view=(sum(pv)/sum(uv)),logisticscore=mean(logisticscore),
-                     orderscore=mean(orderscore),servicescore=mean(servicescore), availability=sum(availability)),by=.(date, brand_id, sub_category_id)]
 #Simgenin kodu
 
-tr_sc_brand[,day:=weekdays(tr_sc_brand$date, abbreviate = TRUE)]
-dummy=tr_sc_brand[,.(quantity=sum(quantity)),by=c("date","day")]
-ggplot(tr_sc_brand[,.(quantity=sum(quantity)),by=c("date","day")],aes(x=date,y=quantity, colour=day, group=day))+geom_line()+geom_point()
+tr_sc[,day:=weekdays(tr_sc$date, abbreviate = TRUE)]
+dummy=tr_sc[,.(quantity=sum(quantity)),by=c("date","day")]
+ggplot(tr_sc[,.(quantity=sum(quantity)),by=c("date","day")],aes(x=date,y=quantity, colour=day, group=day))+geom_line()+geom_point()
 ggplot(dummy[quantity<10000],aes(x=date,y=quantity, colour=day, group=day))+geom_line()+geom_point()
 
-#tr_sc_brand[day=="Sun", if_Sun:=1]
-#tr_sc_brand[day!="Sun", if_Sun:=0]
+#AsÃ½l Ã¶nemli kÃ½sÃ½m
+tr_sc_brand=tr_sc[,.(quantity=sum(quantity),price=mean(price),view=(sum(pvs)/sum(uvs)),logisticscore=mean(logisticscore),
+                     orderscore=mean(orderscore),servicescore=mean(servicescore), availability=sum(availability),merchnumless950,day,uv=sum(uvs),pv=sum(pvs)),by=.(date, brand_id, sub_category_id)]
 
-tr_sc_brand[day=="Paz", if_Sun:=1]
-tr_sc_brand[day!="Paz", if_Sun:=0]
+tr_sc_brand=unique(tr_sc_brand)
 
-#tr_sc_brand[day=="Mon", if_Mon:=1]
-#tr_sc_brand[day!="Mon", if_Mon:=0]
 
-tr_sc_brand[day=="Pzt", if_Mon:=1]
-tr_sc_brand[day!="Pzt", if_Mon:=0]
+tr_sc_brand[day=="Sun", if_Sun:=1]
+tr_sc_brand[day!="Sun", if_Sun:=0]
 
-#tr_sc_brand[day=="Tue", if_Tue:=1]
-#tr_sc_brand[day!="Tue", if_Tue:=0]
+#tr_sc_brand[day=="Paz", if_Sun:=1]
+#tr_sc_brand[day!="Paz", if_Sun:=0]
 
-tr_sc_brand[day=="Sal", if_Tue:=1]
-tr_sc_brand[day!="Sal", if_Tue:=0]
+tr_sc_brand[day=="Mon", if_Mon:=1]
+tr_sc_brand[day!="Mon", if_Mon:=0]
 
-#tr_sc_brand[day=="Wed", if_Wed:=1]
-#tr_sc_brand[day!="Wed", if_Wed:=0]
+#tr_sc_brand[day=="Pzt", if_Mon:=1]
+#tr_sc_brand[day!="Pzt", if_Mon:=0]
 
-tr_sc_brand[day=="Çar", if_Wed:=1]
-tr_sc_brand[day!="Çar", if_Wed:=0]
+tr_sc_brand[day=="Tue", if_Tue:=1]
+tr_sc_brand[day!="Tue", if_Tue:=0]
 
-#tr_sc_brand[day=="Thu", if_Thu:=1]
-#tr_sc_brand[day!="Thu", if_Thu:=0]
+# tr_sc_brand[day=="Sal", if_Tue:=1]
+# tr_sc_brand[day!="Sal", if_Tue:=0]
 
-tr_sc_brand[day=="Per", if_Thu:=1]
-tr_sc_brand[day!="Per", if_Thu:=0]
+tr_sc_brand[day=="Wed", if_Wed:=1]
+tr_sc_brand[day!="Wed", if_Wed:=0]
 
-#tr_sc_brand[day=="Fri", if_Fri:=1]
-#tr_sc_brand[day!="Fri", if_Fri:=0]
+# tr_sc_brand[day=="Ã‡ar", if_Wed:=1]
+# tr_sc_brand[day!="Ã‡ar", if_Wed:=0]
 
-tr_sc_brand[day=="Cum", if_Fri:=1]
-tr_sc_brand[day!="Cum", if_Fri:=0]
+tr_sc_brand[day=="Thu", if_Thu:=1]
+tr_sc_brand[day!="Thu", if_Thu:=0]
 
-#tr_sc_brand[day=="Sat", if_Sat:=1]
-#tr_sc_brand[day!="Sat", if_Sat:=0]
+# tr_sc_brand[day=="Per", if_Thu:=1]
+# tr_sc_brand[day!="Per", if_Thu:=0]
 
-tr_sc_brand[day=="Cmt", if_Sat:=1]
-tr_sc_brand[day!="Cmt", if_Sat:=0]
+tr_sc_brand[day=="Fri", if_Fri:=1]
+tr_sc_brand[day!="Fri", if_Fri:=0]
 
-#Benim R Türkçe çıktı da :)
+# tr_sc_brand[day=="Cum", if_Fri:=1]
+# tr_sc_brand[day!="Cum", if_Fri:=0]
+
+tr_sc_brand[day=="Sat", if_Sat:=1]
+tr_sc_brand[day!="Sat", if_Sat:=0]
+
+# tr_sc_brand[day=="Cmt", if_Sat:=1]
+# tr_sc_brand[day!="Cmt", if_Sat:=0]
+
+#Benim R TÃ¼rkÃ§e Ã§Ã½ktÃ½ da :)
 
 
 #dummy=tr_sc_brand[,list(brand_id,sub_category_id),by="date"]
@@ -105,9 +111,7 @@ tr_sc_brand[day!="Cmt", if_Sat:=0]
 
 #dummy=tr_sc_brand[brand_id==232 & sub_category_id==250]
 #dummy=dummy[order(date)]
-#dummy[,cumlogisticscore:=cumsum(logisticscore)/(1:.N)]
-
-#Şurayı üşenmeden yazana alkış (alkış emojisi)
+#dummy[,cumlogisticscore:=cumsum(logisticscore)/(1:.N)
 
 tr_sc_brand=tr_sc_brand[order(date)]
 
@@ -122,22 +126,64 @@ tr_sc_brand[brand_id==773 & sub_category_id==117, cumlogisticscore:=cumsum(logis
 tr_sc_brand[brand_id==773 & sub_category_id==250, cumlogisticscore:=cumsum(logisticscore)/(1:.N)]
 tr_sc_brand[brand_id==773 & sub_category_id==376, cumlogisticscore:=cumsum(logisticscore)/(1:.N)]
 tr_sc_brand[brand_id==850 & sub_category_id==117, cumlogisticscore:=cumsum(logisticscore)/(1:.N)]
-
-#Ayrıca Nesliciğe de alkış bence arada unutulanlar olmuş.
 tr_sc_brand[brand_id==659 & sub_category_id==376, cumlogisticscore:=cumsum(logisticscore)/(1:.N)]
 tr_sc_brand[brand_id==609 & sub_category_id==285, cumlogisticscore:=cumsum(logisticscore)/(1:.N)]
 tr_sc_brand[brand_id==285 & sub_category_id==250, cumlogisticscore:=cumsum(logisticscore)/(1:.N)]
 
+tr_sc_brand[brand_id==232 & sub_category_id==250, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==345 & sub_category_id==365, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==609 & sub_category_id==9, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==609 & sub_category_id==44, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==609 & sub_category_id==117, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==609 & sub_category_id==376, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==773 & sub_category_id==69, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==773 & sub_category_id==117, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==773 & sub_category_id==250, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==773 & sub_category_id==376, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==850 & sub_category_id==117, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==659 & sub_category_id==376, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==609 & sub_category_id==285, cumorderscore:=cumsum(orderscore)/(1:.N)]
+tr_sc_brand[brand_id==285 & sub_category_id==250, cumorderscore:=cumsum(orderscore)/(1:.N)]
+
+tr_sc_brand[brand_id==232 & sub_category_id==250, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==345 & sub_category_id==365, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==609 & sub_category_id==9, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==609 & sub_category_id==44, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==609 & sub_category_id==117, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==609 & sub_category_id==376, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==773 & sub_category_id==69, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==773 & sub_category_id==117, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==773 & sub_category_id==250, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==773 & sub_category_id==376, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==850 & sub_category_id==117, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==659 & sub_category_id==376, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==609 & sub_category_id==285, cumservicescore:=cumsum(servicescore)/(1:.N)]
+tr_sc_brand[brand_id==285 & sub_category_id==250, cumservicescore:=cumsum(servicescore)/(1:.N)]
 
 
-#Datamız artık hazır. Yaşasın :)
+#DatamÃ½z artÃ½k hazÃ½r. YaÃ¾asÃ½n :)
 
 #specify sub_category_id and brand_id inside the regression model
-sc_250_fit_=lm(formula = quantity~logisticscore+orderscore+servicescore+price+view+beg_inv, data = tr_sc_brand[sub_category_id==250 & brand_id==232,])
-summary(sc_250_fit_)
-# 
-# #stepwise regression
-# sc_250_fit_step=step(lm(formula = quantity~logisticscore+orderscore+servicescore+price+view+beg_inv, data = tr_sc_brand[sub_category_id==250 & brand_id==232,]))
+
+data<-tr_sc_brand[brand_id==232 & sub_category_id==250,]
+#lagged data
+#datayÄ± bozmamak iÃ§in temp Ã¼zerinde deÄŸiÅŸiklik yaptÄ±m
+temp=tr_sc_brand[sub_category_id==250 & brand_id==232,]
+temp[,view := shift(view, 1, type="lag")]
+temp[,price := shift(price, 1, type="lag")]
+temp[,cumlogisticscore := shift(cumlogisticscore, 1, type="lag")]
+temp[,cumorderscore := shift(cumorderscore, 1, type="lag")]
+temp[,cumservicescore := shift(cumservicescore, 1, type="lag")]
+temp[,merchnumless950 := shift(merchnumless950, 1, type="lag")]
+temp[,availability := shift(availability, 1, type="lag")]
+temp[,uv := shift(uv, 1, type="lag")]
+temp[,pv := shift(pv, 1, type="lag")]
+
+#Time for lags! price, maxprice, minprice, avgprice, cumlogisticscore,cumservicescore,cumorderscore,
+
+#Stepwise Regression
+
+# fit=step(lm(formula = quantity~logisticscore+orderscore+servicescore+price+view+beg_inv, data = tr_sc_brand[sub_category_id==250 & brand_id==232,]))
 # summary(sc_250_fit_step)
 # 
 # #to scale the beg_inv
@@ -163,7 +209,3 @@ tr_sc_brand[, mrktpriceSC:= mean(price), by=c("date", "sub_category_id")]
 
 
 inventory[, beg_inv+replen_in+trans_in==0, ]
-
-
-
-
